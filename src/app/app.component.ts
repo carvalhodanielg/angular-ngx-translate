f4b5f55dcfc7de469302b96e173e3baceacf55ad
carvalhodanielg <carvalhodanielg@gmail.com>
2024-07-02 21:52:46 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,136 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
+import {
+  LangChangeEvent,
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    MenubarModule,
+    AvatarModule,
+    BadgeModule,
+    CommonModule,
+    TranslateModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [ConfirmationService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'testAngular';
+
+  items: MenuItem[] | undefined;
+  lang: string = 'pt';
+
+  constructor(
+    public translate: TranslateService,
+    public primeNGConfig: PrimeNGConfig,
+    private confirmationService: ConfirmationService
+  ) {
+    translate.addLangs(['en', 'pt']);
+    translate.setDefaultLang('pt');
+
+    const browserLang = translate.getBrowserLang();
+    let lang = browserLang?.match(/en|pt/) ? browserLang : 'en';
+    this.changeLang(lang);
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
+
+  buildUserProfileMenu(): MenuItem[] {
+    const menu: MenuItem[] = [
+      {
+        label: this.translate.instant('menu.home'),
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Set PT',
+        icon: 'pi pi-star',
+        command: () => {
+          this.changeLang('pt');
+        },
+      },
+      {
+        label: 'Set EN',
+        icon: 'pi pi-star',
+        command: () => {
+          this.changeLang('en');
+        },
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Core',
+            icon: 'pi pi-bolt',
+            shortcut: '⌘+S',
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server',
+            shortcut: '⌘+B',
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
+            shortcut: '⌘+U',
+          },
+          {
+            separator: true,
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+                badge: '2',
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+                badge: '3',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+        badge: '3',
+      },
+    ];
+    return menu;
+  }
+  languageSubscription: any;
+
+  ngOnInit(): void {
+    this.languageSubscription = this.translate.onLangChange.subscribe(
+      (event: LangChangeEvent) => (this.items = this.buildUserProfileMenu())
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,47 @@
+/// <reference types="@angular/localize" />
+import { Component, importProvidersFrom } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
+
+bootstrapApplication(
+  AppComponent,
+  {
+    providers: [
+      provideRouter(routes),
+      provideHttpClient(),
+      importProvidersFrom(
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient],
+          },
+        })
+      ),
+    ],
+  }
+  // TranslateModule.forRoot({
+  // loader: {
+  //   provide: TranslateLoader,
+  //   useFactory: HttpLoaderFactory,
+  //   deps: [HttpClient],
+  // },
+  // })
+).catch((err) => console.error(err));
